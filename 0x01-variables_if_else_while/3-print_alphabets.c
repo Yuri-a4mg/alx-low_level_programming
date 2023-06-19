dd7f74da2f0bdf9f68f006f7391f64c164fd25c0
Youssra <youssra.amg@gmail.com>
2023-06-19 11:29:04 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -5,16 +5,15 @@
  *
  * Return: Always 0 (Success)
  */
-
-
 int main(void)
 {
-char alp[52] ="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+char alp[52] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
 	int i;
+
 	for (i = 0; i < 52; i++)
 	{
 		putchar(alp[i]);
 	}
 	putchar('\n');
-	rerturn (0);
+	return (0);
 }
