1bc0f518207228973c374cb2f06a07b983b21328
Youssra <youssra.amg@gmail.com>
2023-06-19 10:26:27 -0700

    2-print_alphabet.c
    git add .
    vi j

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 /**
- * main - Prints the alphabetic
- * Return: Always (Successà
+ * main - Prints the alphabet
  *
+ * Return: Always 0(Success)
  */
 int main(void)
 {
-	char c;
-	for (c = 'a'; c <= 'z'; c++)
-	putchar(c);
+	char alp[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alp[i]);
+	}
 	putchar('\n');
 	return (0);
 }
