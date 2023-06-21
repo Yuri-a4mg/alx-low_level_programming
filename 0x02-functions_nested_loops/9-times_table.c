470a18e3ede416d22f1db9abc340d25f964d3fd8
Youssra <youssra.amg@gmail.com>
2023-06-21 14:18:41 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -4,36 +4,34 @@
  * times_table - Prints the 0 times table, strating with 0
  * Return: empty output
  */
-
 void times_table(void)
-
 {
-	int x, y, z, u, d;
+	int x, y, z;
 
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x < 10; x++)
 	{
-	for (y = 0; y <= 9; y++)
+	for (y = 0; y < 10; y++)
 	{
 	z = x * y;
-	if (z > 9)
+	if (y == 0)
 	{
-	u = z % 10;
-	d = (z - u) / 10;
-	_putchar(44);
-	_putchar(32);
-	_putchar(d + '0');
-	_putchar(u + '0');
+	_putchar(z  + '0');
 	}
-	else
+
+	if (z < 10 && y != 0)
 	{
-	if (y != 0)
+		_putchar(',');
+		_putchar(' ');
+		_putchar(' ');
+		_putchar(z + '0');
+	} else if (z >= 10)
 	{
-	_putchar(44);
-	_putchar(32);
-	_putchar(32);
-	}
-	putchar(z + '0');
+		_putchar(',');
+		_putchar(' ');
+		_putchar((z / 10) + '0');
+		_putchar((z % 10) + '0');
 	}
 	}
 	_putchar('\n');
+	}
 }
