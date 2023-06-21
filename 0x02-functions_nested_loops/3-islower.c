b481279ee98935c8d8e69d729c4157b2c5edb740
Youssra <youssra.amg@gmail.com>
2023-06-21 12:23:41 -0700

    4-isalpha.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,15 +3,18 @@
 /**
  * _islower – Checks for lowercase character
  * @c: The character to be checked
+ * 
  * Return: 1 for lowercase character or 0 for anything else
  */
 
 int _islower(int c)
-
 {
-	if (c >= 97 && c <= 122)
+	if (c >= 'a' && c <= 'z')
 	{
-	return (1);
+		return (1);
 	}
-	return (0);
+	else
+	{
+		return (0);
+	}
 }
