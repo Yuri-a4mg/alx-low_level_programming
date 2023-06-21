cc5079ee9ae8e81e64d76d71014ffd81c4a601a8
Youssra <youssra.amg@gmail.com>
2023-06-21 10:11:48 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *print_alphabet - Make the alphabet
+ * Return: void
+ */
+int print_alphabet(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+}
