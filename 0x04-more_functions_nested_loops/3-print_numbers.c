203d7a0e9091a430b51e2e21bb10a723cbe1af16
Youssra <youssra.amg@gmail.com>
2023-06-23 12:05:00 -0700

    f

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,16 +2,16 @@
 
 /**
  * print_numbers - print 0-9
- * return: the numbers since 0 up to 9
+ * return: void
  */
 
 void print_numbers(void)
 {
-	int x;
+	char c;
 
-	for (x = 0; x <= 9; x++)
+	for (c = '0'; c <= '9'; c++)
 	{
-		_putchar(x + '0');
+		_putchar(c);
 	}
 	_putchar('\n');
 }
--- 0x04-more_functions_nested_loops/4-main.c
+++ 0x04-more_functions_nested_loops/4-main.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * #include "main.h"
+ * main - check the code
  *
  * Return: Always 0.
  */
